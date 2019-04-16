0138adef338d0448bb99316acd5d0aab5da804cb
Shalasay <49329939+Shalasay@users.noreply.github.com>
2019-04-15 23:12:36 -0500

    Fix for req update

--- p01manage.php
+++ p01manage.php
@@ -12,7 +12,8 @@ echo("<h1> Account Management</h1>");
 		echo("
   <form method=\"post\" action=\"p01manage.php?sessionid=$sessionid\">
   Username: <input type=\"text\" size=\"10\" maxlength=\"20\" name=\"q_clientid\"> 
-  Account Type: <input type=\"text\" size=\"20\" maxlength=\"50\" name=\"q_clienttype\">
+  Admin flag: <input type=\"text\" size=\"20\" maxlength=\"50\" name=\"q_aflag\">
+  Student flag: <input type=\"text\" size=\"20\" maxlength=\"50\" name=\"q_sflag\">
   <input type=\"submit\" value=\"Search\">
   </form>
 
@@ -27,8 +28,8 @@ echo("<h1> Account Management</h1>");
   
 	// Interpret the query requirements
 $q_clientid = $_POST["q_clientid"];
-//$q_password = $_POST["q_password"];
-$q_clienttype = $_POST["q_clienttype"];
+$q_aflag = $_POST["q_aflag"];
+$q_sflag = $_POST["q_sflag"];
 
 $whereClause = " 1=1 ";
 
@@ -36,15 +37,15 @@ if (isset($q_clientid) and $q_clientid!= "") {
   $whereClause .= " and clientid like '$q_clientid%'"; 
 }
 
-//if (isset($q_password) and $q_password!= "") { 
-//  $whereClause .= " and password like '%$q_password%'"; 
-//}
 
-if (isset($q_clienttype) and $q_clienttype!= "") { 
-  $whereClause .= " and clienttype like '%$q_clienttype%'"; 
+if (isset($q_aflag) and $q_aflag!= "") {
+  $whereClause .= " and aflag like '%$q_aflag%'"; 
+}
+if (isset($q_sflag) and $q_sflag!= "") {
+  $whereClause .= " and sflag like '%$q_sflag%'"; 
 }
 // Form the query and execute it
-$sql = "select clientid, password, clienttype from p01users where $whereClause order by clientid";
+$sql = "select clientid, password, aflag, sflag from p01users where $whereClause order by clientid";
 //echo($sql);
 
 $result_array = execute_sql_in_oracle ($sql);
@@ -59,16 +60,17 @@ if ($result == false){
 
 // Display the query results
 echo "<table border=1>";
-echo "<tr> <th>Username</th> <th>Password</th> <th>Account Type</th> <th>Update</th> <th>Delete</th></tr>";
+echo "<tr> <th>Username</th> <th>Password</th> <th>Admin Flag</th> <th>Student Flag</th> <th>Update</th> <th>Delete</th></tr>";
 
 // Fetch the result from the cursor one by one
 while ($values = oci_fetch_array ($cursor)){
   $clientid = $values[0];
   $password = $values[1];
-  $clienttype = $values[2];
+  $aflag = $values[2];
+  $sflag = $values[3];
   echo("<tr>" . 
-    "<td>$clientid</td> <td>$password</td> <td>$clienttype</td> ".
-    " <td> <A HREF=\"p01acc_update.php?sessionid=$sessionid&clientid=$clientid&password=$password&type=$clienttype\">Update</A> </td> ".
+    "<td>$clientid</td> <td>$password</td> <td>$aflag</td> <td>$sflag</td> ".
+    " <td> <A HREF=\"p01acc_update.php?sessionid=$sessionid&clientid=$clientid&password=$password&aflag=$aflag&sflag=$sflag\">Update</A> </td> ".
 	" <td> <A HREF=\"p01user_delete.php?sessionid=$sessionid&clientid=$clientid\">Delete</A> </td> ".
     "</tr>");
 }					
@@ -89,7 +91,7 @@ $sql = "select clientid " .
 	  die("SQL Execution problem.");
   }	
   else{
-	  $sql = "select clienttype " .
+	  $sql = "select aflag, sflag " .
 		"from p01users natural join p01myclientsession " .
 		"where sessionid = '$sessionid'";
 		
@@ -102,25 +104,27 @@ $sql = "select clientid " .
 		die("SQL Execution problem.");
 		}	
 	  if($values = oci_fetch_array($cursor)){
-				if(strcasecmp((string)$values[0], 'admin') != 0 && strcasecmp((string)$values[0], 'stuadmin') != 0){
-				  echo("<FORM action=\"p01stuwelcomepage.php?sessionid=$sessionid\" name=\"Main Page \" method=\"POST\"> " .
-									"<INPUT type=\"submit\" name=\"btnSubmit\" value=\"Main Page\"> " .
-								"</FORM>"); 
-								}
-								else if(strcasecmp((string)$values[0], 'stu') != 0 && strcasecmp((string)$values[0], 'stuadmin') != 0){
-				  echo("<FORM action=\"p01adminwelcomepage.php?sessionid=$sessionid\" name=\"Main Page \" method=\"POST\"> " .
-									"<INPUT type=\"submit\" name=\"btnSubmit\" value=\"Main Page\"> " .
-								"</FORM>"); 
-				}	
-			else if(strcasecmp((string)$values[0], 'admin') != 0 && strcasecmp((string)$values[0], 'stu') != 0){
-				  echo("<FORM action=\"p01stuadminwelcomepage.php?sessionid=$sessionid\" name=\"Main Page \" method=\"POST\"> " .
-									"<INPUT type=\"submit\" name=\"btnSubmit\" value=\"Main Page\"> " .
-								"</FORM>"); 
+				if($values[0] == 1){
+					if($values[1] == 1){
+						echo("<FORM action=\"p01stuadminwelcomepage.php?sessionid=$sessionid\" name=\"Main Page \" method=\"POST\"> " .
+					   "<INPUT type=\"submit\" name=\"btnSubmit\" value=\"Main Page\"> " .
+					   "</FORM>");
+					}else{
+						echo("<FORM action=\"p01adminwelcomepage.php?sessionid=$sessionid\" name=\"Main Page \" method=\"POST\"> " .
+					   "<INPUT type=\"submit\" name=\"btnSubmit\" value=\"Main Page\"> " .
+					   "</FORM>");
+					}
+				}else if($values[0] == 0){
+					if($values[1] == 1){
+						echo("<FORM action=\"p01stuwelcomepage.php?sessionid=$sessionid\" name=\"Main Page \" method=\"POST\"> " .
+					   "<INPUT type=\"submit\" name=\"btnSubmit\" value=\"Main Page\"> " .
+					   "</FORM>");
+					}else{
+						die ('An Error has occurred.  Click <A href="p01login.html">here</A> to go back to the login page.');
+					}
 				}
 		  }
-			 
-	}					
-		
+	}
 oci_free_statement($cursor);
 
 ?>
